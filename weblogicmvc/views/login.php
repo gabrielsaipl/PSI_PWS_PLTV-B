0af548dbbce4f918bcd6e12a363027fca8fa5299
Gabriel <2211901@my.ipleiria.pt>
2022-06-08 11:36:18 +0100

    Alteracao de username para email na autenticacao do site

--- weblogicmvc/views/login.php
+++ weblogicmvc/views/login.php
@@ -1,10 +1,10 @@
 
 <div class="login-page">
     <div class="form">
-    <form class="login-form" method="post" action="router.php?c=login&a=login">
+    <form class="login-form" method="post" action="index.php?c=login&a=login">
         <h2>Efetuar Login</h2>
-      <input type="text" id="username" name="name" placeholder="username" required placeholder="Enter Name" oninvalid="this.setCustomValidity('Introduza o nome de utilizador')" oninput="this.setCustomValidity('')"/>
-      <input type="password" id="password" name="password" placeholder="password" required placeholder="Enter Name" oninvalid="this.setCustomValidity('Introduza o nome de utilizador')" oninput="this.setCustomValidity('')"/>
+      <input type="text" id="email" name="email" placeholder="email" required oninvalid="this.setCustomValidity('Introduza o seu email')" oninput="this.setCustomValidity('')"/>
+      <input type="password" id="password" name="password" placeholder="palavra-passe" required oninvalid="this.setCustomValidity('Introduza a sua password')" oninput="this.setCustomValidity('')"/>
       <button>login</button>
     </form>
   </div>
